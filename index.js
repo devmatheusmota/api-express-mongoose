ec2edf98889e97b204ef6a3a6ed87a2d53e111c9
Matheus Mota <devmatheusmota@gmail.com>
2022-06-24 15:03:52 -0300

    Added all routes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // initial config
+
 const express = require('express');
+const mongoose = require('mongoose');
+const { eventNames } = require('./models/Person');
 const app = express();
+require('dotenv').config();
 
 //config to read JSON
 
@@ -12,10 +16,25 @@ app.use(
 
 app.use(express.json());
 
-//endpoints
+//routes
+const personRoutes = require('./routes/personRoutes');
+
+app.use('/person', personRoutes);
+
 app.get('/', (request, response) => {
 	response.json({ message: 'Hello World!' });
 });
 
-//deliver a port
-app.listen(3000);
+//deliver a port and connecting to DB
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const connectString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.flydf.mongodb.net/apiDB?retryWrites=true&w=majority`;
+
+mongoose
+	.connect(connectString)
+	.then(() => {
+		console.log('MongoDB connected.');
+		app.listen(3000);
+	})
+	.catch((e) => console.log(e));
---
+++ routes/personRoutes.js
@@ -0,0 +1,96 @@
+const router = require('express').Router();
+
+const { update } = require('../models/Person');
+const Person = require('../models/Person');
+
+//Create
+router.post('/', async (request, response) => {
+	const { name, salary, approved } = request.body;
+
+	if (!name) {
+		return response.status(422).json({ error: 'Name is mandatory!' });
+		return;
+	} else if (!salary) {
+		return response.status(422).json({ error: 'Salary is mandatory!' });
+	} else if (typeof approved === 'undefined') {
+		return response
+			.status(422)
+			.json({ error: 'Approved status is mandatory!' });
+		return;
+	}
+
+	const person = {
+		name,
+		salary,
+		approved,
+	};
+
+	try {
+		await Person.create(person);
+		response.status(201).json({ message: 'Person inserted.' });
+	} catch (error) {
+		response.status(500).json({ error: error });
+	}
+});
+
+//Read
+router.get('/', async (request, response) => {
+	try {
+		const people = await Person.find();
+		return response.status(200).json(people);
+	} catch (error) {
+		return response.status(500).json({ error: error });
+	}
+});
+
+//Read by Id
+router.get('/:id', async (request, response) => {
+	const { id } = request.params;
+
+	try {
+		const person = await Person.findById(id);
+		if (!person) {
+			return response.status(422).json({ message: 'User not found!' });
+		}
+		return response.status(200).json(person);
+	} catch (error) {
+		return response.status(500).json({ error: error });
+	}
+});
+
+//Update
+router.patch('/:id', async (request, response) => {
+	const { id } = request.params;
+
+	const person = ({ name, salary, approved } = request.body);
+
+	try {
+		const updatedPerson = await Person.updateOne({ _id: id }, person);
+		console.log(updatedPerson);
+		if (updatedPerson.matchedCount === 0) {
+			return response.status(422).json({ message: 'User not found!' });
+		}
+		return response.status(200).json(person);
+	} catch (error) {
+		return response.status(500).json({ error: error });
+	}
+});
+
+//Delete
+router.delete('/:id', async (request, response) => {
+	const { id } = request.params;
+
+	const person = await Person.findById(id);
+
+	if (!person) {
+		return response.status(422).json({ error: 'User not found!' });
+	}
+	try {
+		await Person.deleteOne({ _id: id });
+		return response.status(200).json({ message: 'User successfully deleted!' });
+	} catch (error) {
+		return response.status(500).json({ error: error });
+	}
+});
+
+module.exports = router;

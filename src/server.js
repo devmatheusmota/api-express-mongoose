6fa2ebf64f357f8e0408740b879f03da0f7ec470
Matheus Mota <devmatheusmota@gmail.com>
2022-10-31 12:32:19 -0300

    Change connection string

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.get('/', (request, response) => {
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const connectString = `mongodb://${DB_USER}:${DB_PASSWORD}@docdb-2022-10-31-14-43-38.cluster-cpnsiy7hsa9v.sa-east-1.docdb.amazonaws.com:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+const connectString = `mongodb://matheusmota1996:27098912@docdb-2022-10-31-14-43-38.cluster-cpnsiy7hsa9v.sa-east-1.docdb.amazonaws.com:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
 
 mongoose
 	.connect(connectString, {
